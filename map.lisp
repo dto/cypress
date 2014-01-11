4d6b292d6e1150b28fbe0ddaf0c89a82781cbca1
David O'Toole <blocky.io>
2014-01-11 05:57:46 -0500

    map is now grid based

--- map.lisp
+++ map.lisp
@@ -5,7 +5,7 @@
 (defparameter *frozen-meadow-icons* (image-set "frozen" 2))
 (defparameter *danger-icons* (image-set "danger" 2))
 (defparameter *forest-icons* (image-set "forest" 6))
-(defparameter *frozen-forest-icons* (image-set "frozen-forest" 6))
+(defparameter *frozen-forest-icons* (image-set "frozen-forest" 2))
 (defparameter *grassland-icons* (image-set "grassland" 3))
 (defparameter *grassy-meadow-icons* (image-set "grassy-meadow" 2))
 (defparameter *large-mountain-icons* (image-set "large-mountain" 3))
@@ -17,13 +17,14 @@
 (defparameter *home-image* "home.png")
 (defparameter *road-image* "road.png")
 
-(defparameter *sector-size* (units 5))
+(defparameter *sector-size* (units 6))
 
 (defthing sector
   :tags '(:fixed)
   :terrain nil
   :scale 1.1
-  :image (random-choose *grassy-meadow-icons*))
+  :height *sector-size*
+  :width *sector-size*)
 
 (defparameter *terrain-classes* '(forest frozen-forest meadow
   grassy-meadow cold-meadow frozen-meadow ruins river valisade highway
@@ -39,7 +40,7 @@
 	'ruins *ruins-icons*
 	'river *river-icons*
 	'valisade *castle-icons*
-	'highway *road-image*
+	'highway (list *road-image*)
 	'cemetery *danger-icons*
 	;; can't visit these.
 	'home (list *home-image*)
@@ -50,9 +51,9 @@
   (random-choose (getf *terrain-icons* terrain)))
 
 (defmethod initialize ((sector sector) &key terrain)
-  (change-image sector (terrain-icon terrain))
-  (setf (field-value :terrain sector) terrain)
-  (resize sector *sector-size* *sector-size*))
+  (resize sector *sector-size* *sector-size*)
+  (setf (field-value :image sector) (terrain-icon terrain))
+  (setf (field-value :terrain sector) terrain))
 
 (defmethod find-description ((sector sector))
   (pretty-string (symbol-name (field-value :terrain sector))))
@@ -61,40 +62,64 @@
   (with-fields (x y width height image) sector
     (let ((image-width (image-width image))
 	  (image-height (image-height image)))
-	;; (if (> image-width image-height)
-	;;     (draw-image image x y 
-	;; 		:width width
-	;; 		:height (- height 
-	;; 			   (* height (/ width image-width))))
+	(if (> image-width image-height)
+	    (draw-image image x y 
+			:width width
+			:height (- height 
+				   (* height (/ width image-width))))
 	    (draw-image image x y
 			:height height
-			:width (- width (* width (/ height image-height)))))))
+			:width (- width (* width (/ height image-height))))))))
 
 (defmethod activate ((sector sector))
   (with-fields (terrain) sector 
     (switch-to-buffer (new terrain))))
 
-(defun make-sectors (&rest keys)
-  (mapcar #'(lambda (key)
-	      (singleton (new 'sector :terrain key)))
-	  keys))
+(defun make-sector (key)
+  (new 'sector :terrain key))
 
-(defun row-of-sectors (&rest keys)
-  (apply #'lined-up (apply #'make-sectors keys)))
+(defun test-map-sectors ()
+  (list
+   (mapcar #'make-sector '(home meadow forest cold-meadow forest mountain
+			   forest large-mountain frozen-meadow river large-mountain mountain))
+   (mapcar #'make-sector '(meadow grassy-meadow meadow forest forest cold-meadow ruins
+			   frozen-meadow cemetery mountain river frozen-forest mountain))
+   (mapcar #'make-sector '(grassy-meadow meadow forest forest mountain
+			   forest highway cemetery ruins river frozen-meadow river
+			   valisade))
+   (mapcar #'make-sector '(meadow grassy-meadow forest forest 
+			   cold-meadow ruins highway frozen-meadow frozen-forest river river))))
+			   
+	  
 
 (defthing (map-screen buffer)
+  :sectors nil
   :background-image "parchment.png")
 
-(defmethod make-terrain ((map map-screen))
-  (stacked-up
-   (row-of-sectors 'grassy-meadow 'meadow 'forest 'cold-meadow 'forest 'mountain 'forest 'large-mountain 'frozen-meadow 'river 'large-mountain 'mountain)
-   (row-of-sectors 'meadow 'grassy-meadow 'meadow 'forest 'forest 'cold-meadow 'ruins 'frozen-meadow 'cemetery 'mountain 'river 'valisade 'mountain)
-   (row-of-sectors 'home 'meadow 'forest 'forest 'mountain 'forest 'cold-meadow 'cemetery 'ruins 'river 'frozen-meadow 'river 'mountain)))
+(defparameter *map-screen-left-margin* 100)
+(defparameter *map-screen-top-margin* 230)
+
+(defmethod arrange ((map-screen map-screen))
+  ;; lay out the items
+  (let ((x0 *map-screen-left-margin*)
+	(y0 *map-screen-top-margin*)
+	(sector-rows (field-value :sectors map-screen)))
+    (dotimes (row (length sector-rows))
+      (let ((sectors (pop sector-rows)))
+	(dotimes (column (length sectors))
+	  (let ((sector (pop sectors)))
+	    (resize sector *sector-size* *sector-size*)
+	    (move-to sector 
+		     (+ x0 (* column *sector-size*))
+		     (+ y0 (* row *sector-size*)))))))))
 
 (defmethod initialize :after ((map map-screen) &key)
-  (paste-from map (make-terrain map) 130 250)
-  (resize map 1280 781))
-  
+  (with-fields (sectors) map 
+    (setf sectors (test-map-sectors))
+    (dolist (row sectors)
+      (dolist (sector row)
+	(drop-object map sector)))
+    (resize map 1280 781)))
 
-
-  
+(defmethod update :after ((map map-screen)) 
+  (arrange map))
