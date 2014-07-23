eb59a042c351aa5ca0a5081447c4361140560bf1
David O'Toole <blocky.io>
2014-07-23 11:56:37 -0400

    expanded conversation from maxwell

--- townspeople.lisp
+++ townspeople.lisp
@@ -29,10 +29,6 @@
 	(let ((targets (find-instances (current-scene) 'stone-patio)))
 	  (when targets (random-choose targets)))))
 
-;; (defmethod return-home ((self maxwell))
-;;   (multiple-value-bind (x y) (below (find-pentaquin-house))
-;;     (walk-to self x y)))
-
 (defmethod run ((self maxwell))
   (with-fields (next-target gump waypoints) self
     (call-next-method)
@@ -59,11 +55,18 @@
       (discuss self :busy)))
 
 (define-topic busy maxwell 
-  "Oh, hello! So much to do. I'm
-terribly busy right now. Please come
-back later!" :bye)
+  "Oh, hello! You look interesting. You
+must be from outside the Vale? So much
+to do. I'm terribly busy right now,
+inspecting these ancient stones. The
+town's walls have got to be
+assessed. Repairs will need to be
+made. Budgets and schedules will need to
+be written up! Please come back later."
+:bye)
 
 (define-topic hello maxwell "Good morning!" :name)
+
 (define-topic name maxwell 
   "My name is Maxwell Montana. I work at
 the Geological Society here in town, and
@@ -78,20 +81,21 @@ don't get out of the Valley much."
 :explain-situation)
 
 (define-topic explain-situation maxwell 
-"O, God of Gods! Had you not told me the
-name Valisade, I would have taken you
-for a madman or a charlatan. But I know
-that your story of time-travel is
-true. For I can hear the curious
-similarity of your V and B sounds, and
-the way your terminal vowels tend to
-slur just a little. For I am a man
+"I believe you, Stranger. Had you not
+told me the name Valisade, I would have
+taken you for a madman or a
+charlatan. But I know that your story of
+time-travel is true. For I am a man
 trained in the science of Language, and
 of the changing of its forms over
-time. It seems that \"Valisade\" must
-have been the ancient way of saying what
-today is called \"Balsalva.\" by
-archaeologists." :balsalva)
+time. I can hear the curious similarity
+of your V and B sounds, and the way your
+terminal vowels tend to slur just a
+little. The linguist in me thinks
+\"Valisade\" must have been the ancient
+way of saying what today is called
+\"Balsalva.\" by archaeologists." 
+:balsalva)
 
 (define-topic balsalva maxwell
 "Ahh... I see.
@@ -105,26 +109,32 @@ your friends and loved ones.
 
 Strictly speaking, all the stones I
 catalogue for the Society are ruins of
-the Balsalva. But The Monastery's ruins
-are to the North of here. I'm sorry to
-say that very little remains of the
-Abbey itself. 
+the Balsalva; even the old stone walls
+and patios here in town. But I suspect
+you will want to know about the
+Monastery. I'm sorry to say that very
+little remains of the Abbey itself. The
+ruins are to the North of here.
 
 Something just occurred to me. In the
 Abbey and in a few other places in the
 Vale, there are certain megaliths which
 appear to be seals or coverstones. I
 have always wondered what might lay
-underneath them!  There is some record
-of the Balsalvan monks using a system of
-magic Sigils to seal doors. A Sigil can
-only be unlocked by a key whose symbol
-matches the Sigil. There was a \"Warrior
-symbol\" for lay Brothers, and another
-symbol for ordained Monks; but we have
-never found a way to dispel the magic of
-Sigils. Perhaps a man of your Era will
-be better suited to the task?" :symbol :abbey)
+underneath them! Many have tried, but
+none have succeeded in prying loose the
+stones. There is some record of the
+Balsalvan monks using a system of magic
+Sigils to seal doors. A Sigil can only
+be unlocked by one holding a key whose
+symbol matches the Sigil. There was a
+\"Warrior symbol\" for lay Brothers, and
+another, triangular symbol for ordained
+Monks; but we have never found a way to
+dispel the magic of Sigils. Perhaps a
+man of your Era will be better suited to
+the task?"
+:symbol :abbey)
 
 (define-topic symbol maxwell 
 "The lay Brother's key symbol is a
@@ -133,7 +143,9 @@ through it. The circle represents the
 Warrior's shield, and the hooked line is
 his Spear. Coincidentally, it is one of
 the few ideograms to survive into the
-post-modern Era.
+post-modern Era, and some regiments used
+it to adorn armor, banners, and even
+their skin.
 
 You might find that meditation on this
 symbol can manifest a white light of
@@ -144,31 +156,81 @@ spiritual protection." :abbey)
 the north of town. Beware, Geoffrey.
 Sometimes glowing lights have been seen
 in that clearing from afar; perhaps
-ancient Spirits inhabit those stones.
+ancient Spirits wander among the stones.
 
 Even farther North, at the frozen
 foothills of Mount Pisgah, there is a
-second ruin, perhaps a sister city of
-some kind. It has deteriorated horribly,
+second ruin. It has deteriorated badly,
 but perhaps you will be able to learn
-something there.
+something there. There are probably
+other ruins in the Northwest, but we
+know very little, owing to the extreme
+cold conditions now prevailing there."
+:cold)
 
-You must return soon! If you will but
+(define-topic cold maxwell 
+"Yes. In last century Ildron has seen a
+sharp drop in temperature, and things
+are getting worse. The Sun seems to have
+cooled all of a sudden; we don't know
+why.
+
+The Ancient astrologers collected their
+writings in what is now called the Book
+of the Sun, but only fragments remain of
+this text. We hope that finding more
+pieces will help us understand what is
+happening.
+" :ancients)
+
+(define-topic ancients maxwell
+"You must return soon! If you will but
 allow me to question you for knowledge
 of your Times, and of the people of
-Balsalva. For we at the Library know
-that the world is in a Dark Age; we are
-sworn to preserve the histories of past
-Aeons, in hope that one day a
+Balsalva, a great deal of Good might be
+done. For some of us at the Library
+believe that the world is in a Dark Age;
+we are sworn to preserve the histories
+of past Aeons, in hope that one day a
 newly-prosperous Humankind might balance
 Righteousness with Knowledge. For the
 Dark Age came by the hand of those who
 made Knowledge slave to Righteousness.
 
+Perhaps I spend too much time digging in
+the dirt, to speak of such high
+ideals. But if the wisdom of the
+Ancients can help us survive this age of
+Trial, then what shame is it to have
+dirt beneath one's fingernails?
+
+Oh, and please take this with you. It's
+one of the Ancients' silver books, with
+their special silvery paper that lasts
+forever.  It's a little hand-written
+journal, mostly blank but with a few
+pages of writing at the start. I found
+it in the Northern ruins I told you
+about, and I've carried it since then as
+a good-luck charm. We've never
+deciphered the script, and I didn't have
+the heart to write anything new in
+it. But you might find it useful if
+you're going cross-country, for the book
+and its pages are completely
+water-resistant.
+
 Go in safety, Traveler. If I may fill
-your tankard, let the pages of my
-notebook fill with your stories, when
-you return!" :bye)
+your tankard when you return, then let
+the pages of my notebook fill with your
+stories!" 
+:bye)
+
+(defmethod discuss :after ((maxwell maxwell) (topic (eql :ancients)))
+  (unless (event-occurred-p :maxwell-given-journal)
+    (add-event :maxwell-given-journal)
+    (drop maxwell (new 'journal)
+	  (units 5) (units 5))))
 
 ;;; Madeline
 
