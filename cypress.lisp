d8629d93961d765f21b0f46fed0219ac44fb3161
David O'Toole <blocky.io>
2014-08-12 20:53:22 -0400

    update version

--- cypress.lisp
+++ cypress.lisp
@@ -130,7 +130,7 @@ them onto Geoffrey.")
 ;;; Main game
 
 (defun cypress (&optional (level 1))
-  (setf *window-title* "Cypress v2.0")
+  (setf *window-title* "Cypress v2.1")
   (setf *screen-width* 1280)
   (setf *screen-height* 720)
   (setf *nominal-screen-width* 1680)
